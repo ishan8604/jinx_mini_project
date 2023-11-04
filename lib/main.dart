768510b46bc707201e3c9730411fa9ed54f0463d
ishan8604 <ishan860484@gmail.com>
2023-11-04 11:23:17 +0530

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:jinx/Splash_Screen/splash_Screen.dart';
 import 'package:jinx/Theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: splash_scren(),
